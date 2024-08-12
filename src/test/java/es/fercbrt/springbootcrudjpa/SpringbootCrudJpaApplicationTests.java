f72a8a14c35792e15ede69256f04e76d424d9a25
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-12 17:25:55 +0200

    Initial commit

---
+++ src/test/java/es/fercbrt/springbootcrudjpa/SpringbootCrudJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.fercbrt.springbootcrudjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
