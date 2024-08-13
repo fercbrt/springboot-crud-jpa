764b2b80d39a66340920f736ea92b56f9b4a355c
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-13 09:14:45 +0200

    Added jakarta validation

--- src/main/java/es/fercbrt/springbootcrudjpa/entities/User.java
+++ src/main/java/es/fercbrt/springbootcrudjpa/entities/User.java
@@ -1,6 +1,8 @@
 package es.fercbrt.springbootcrudjpa.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,8 +18,12 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
+    @Size(min = 4, max = 16)
     @Column(name = "name", nullable = false, unique = true)
     private String username;
+    @NotBlank
+    @Size(min = 8, max = 60)
     @Column(nullable = false)
     private String password;
 
