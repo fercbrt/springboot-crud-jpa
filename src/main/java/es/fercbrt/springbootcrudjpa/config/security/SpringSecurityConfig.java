d12be1909f9b150c1e79deb90bddfc230a292523
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-13 10:14:43 +0200

    Updated requestMatchers() from the Security Filter Chain

--- src/main/java/es/fercbrt/springbootcrudjpa/config/security/SpringSecurityConfig.java
+++ src/main/java/es/fercbrt/springbootcrudjpa/config/security/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package es.fercbrt.springbootcrudjpa.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,7 +21,8 @@ public class SpringSecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests( (authz) -> authz
-                .requestMatchers("/api/v1/user/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/v1/user").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/v1/user/register").permitAll()
                 .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement( management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
