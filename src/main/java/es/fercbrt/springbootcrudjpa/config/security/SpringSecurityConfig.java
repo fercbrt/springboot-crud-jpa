9b5b01c6a699cf0639c0f17bf5c14babcf033f19
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-13 09:21:01 +0200

    Configured the security filter chain

--- src/main/java/es/fercbrt/springbootcrudjpa/config/security/SpringSecurityConfig.java
+++ src/main/java/es/fercbrt/springbootcrudjpa/config/security/SpringSecurityConfig.java
@@ -2,8 +2,12 @@ package es.fercbrt.springbootcrudjpa.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -11,5 +15,15 @@ public class SpringSecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests( (authz) -> authz
+                .requestMatchers("/api/v1/user/**").permitAll()
+                .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement( management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 }
